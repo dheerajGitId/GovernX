67e91a8397a65bd52e3259c6cee89a9d2a6fc729
Dheeraj <20211A05E0@bvrit.ac.in>
2026-04-02 11:44:12 +0530

    Caching and Dashboard Metrics Test Failure Correction

--- policyservice/src/test/java/com/clientdata/policyservicetest/service/PolicyServiceIngestTest.java
+++ policyservice/src/test/java/com/clientdata/policyservicetest/service/PolicyServiceIngestTest.java
@@ -3,10 +3,7 @@ package com.clientdata.policyservicetest.service;
 import com.clientdata.policyservice.exception.PolicyServiceException;
 import com.clientdata.policyservice.service.PolicyIngestService;
 import com.clientdata.schemas.model.Customer;
-import com.clientdata.schemas.model.PolicyAudit;
 import com.clientdata.schemas.model.PolicyDocumentBronze;
-import com.clientdata.schemas.model.PolicyUpdateAudit;
-import com.clientdata.schemas.repo.CustomerDetailsRepo;
 import com.clientdata.schemas.repo.PolicyAuditRepo;
 import com.clientdata.schemas.repo.PolicyDocumentBronzeRepo;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,14 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
 
-import static com.clientdata.policyservice.util.PolicyServiceConstants.POLICY_CREATED;
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,8 +25,6 @@ public class PolicyServiceIngestTest {
     @Mock
     PolicyAuditRepo repo2;
 
-    @Mock
-    CustomerDetailsRepo repo3;
 
     PolicyIngestService service;
 
@@ -41,8 +32,7 @@ public class PolicyServiceIngestTest {
     public void setUp() {
         repo = mock(PolicyDocumentBronzeRepo.class);
         repo2 = mock(PolicyAuditRepo.class);
-        repo3 = mock(CustomerDetailsRepo.class);
-        service = new PolicyIngestService(repo, repo2, repo3);
+        service = new PolicyIngestService(repo, repo2);
     }
 
     @Test
@@ -55,8 +45,6 @@ public class PolicyServiceIngestTest {
         customer1.setName("Gary");
         when(repo.existsByPolicyId(anyString())).thenReturn(false);
         when(repo2.existsByAuditId(anyString())).thenReturn(false);
-        when(repo3.existsByCustomerId(customerId)).thenReturn(true);
-        when(repo3.findByCustomerId(customerId)).thenReturn(customer1);
 
         document.setCustomerId(customerId);
 
@@ -64,7 +52,6 @@ public class PolicyServiceIngestTest {
 
         service.PolicyDocumentIngest(document);
         verify(repo, times(1)).save(document);
-        verify(repo3, times(1)).save(customer1);
     }
 
     @Test
@@ -90,11 +77,5 @@ public class PolicyServiceIngestTest {
         assertThrows(PolicyServiceException.class, () -> service.PolicyDocumentIngest(new PolicyDocumentBronze()));
     }
 
-    @Test
-    void testWhenCustomerIsNull() {
-        when(repo3.findByCustomerId(anyString())).thenReturn(null);
-        assertThrows(PolicyServiceException.class, () -> service.PolicyDocumentIngest(new PolicyDocumentBronze()));
-
-    }
 
 }
